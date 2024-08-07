90db7c37112dba3d8fe8f64aeea09438d0269bb8
Suryansh Kumar <95530953+Wizard0880@users.noreply.github.com>
2024-08-07 23:17:48 +0530

    Time: 1 ms (99.55%), Space: 57.1 MB (40.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,40 +1,13 @@
-// class Solution {
-//     public int maxSubArray(int[] nums) {
-//         int maxSum = Integer.MIN_VALUE;
-//         int negSum = Integer.MAX_VALUE;
-//         int currSum = 0;
-//         boolean allNeg = true;
-
-//         for(int i = 0; i < nums.length ; i++){
-//             currSum = currSum + nums[i];
-//             if(currSum < 0){
-//                 currSum = 0;
-//             }
-//             maxSum = Math.max(maxSum,currSum);
-            
-//             if(nums[i] >= 0){
-//                 allNeg = false;
-//             }
-
-//             negSum = Math.max(negSum,nums[i]);
-
-//             if(allNeg){
-//                 return negSum;
-//             }
-//         }
-//         return maxSum;
-//     }
-// }
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
-        int currSum = nums[0];
+        int cs = 0;
+        int max = Integer.MIN_VALUE;
 
-        for(int i = 1; i < nums.length ; i++){
-            currSum = Math.max(nums[i], currSum + nums[i]);
-            maxSum = Math.max(maxSum, currSum);
+        for(int i=0 ; i<nums.length ; i++){
+            cs = cs + nums[i];
+            max = Math.max(cs,max);
+            if(cs < 0) cs = 0;
         }
-
-        return maxSum;
+        return max;
     }
 }
