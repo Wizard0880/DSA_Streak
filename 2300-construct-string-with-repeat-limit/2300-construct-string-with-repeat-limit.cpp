b21337b87e48368d56f595346f1d976f59384651
Suryansh Kumar <95530953+Wizard0880@users.noreply.github.com>
2024-12-17 16:54:10 +0530

    Time: 12 ms (92.15%), Space: 23.7 MB (96.81%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.cpp
@@ -1,49 +1,47 @@
 class Solution {
 public:
     static string repeatLimitedString(string& s, int k) {
-        int freq[26]={0}, hz=0;
-
+        int freq[26] = {0};
         // Count frequency
         for (char c : s) 
             freq[c-'a']++;
         
-        // Use C-array to act as max heap
-        char heap[26];
-        for (int i = 0; i < 26; i++) {
-            if (freq[i]> 0) 
-                heap[hz++]='a'+ i;
+        // Use a max-heap to hold char
+        priority_queue<char> pq;
+        for (int i=0; i<26; i++) {
+            if (freq[i] > 0) 
+                pq.emplace('a'+i);
         }
-        make_heap(heap, heap+hz);
-
+        
         string ans;
         ans.reserve(s.size());
-
-        while (hz > 0) {
-            pop_heap(heap, heap + hz);
-            char c= heap[--hz];
-
+                
+        while (!pq.empty()) {
+            char c=pq.top();
+            pq.pop();
+            
+            // Add as many characters as possible (up to k)
             int& f=freq[c-'a'];
-            int use=min(f, k); // Add as many characters as possible (up to k)
+            int use = min(f, k);
             ans.append(use, c);
             f-= use;
 
-            if (f > 0 && hz > 0) {
-                // Use the next character to interleave
-                pop_heap(heap, heap+hz);
-                char c2=heap[--hz];
-
+            if (f > 0) {
+                if (pq.empty()) 
+                    break; // No other character to interleave
+                
+                // Use the next character in priority queue to interleave
+                char c2= pq.top();
                 ans.push_back(c2);
-                int f2 =--freq[c2 - 'a'];
+                int f2=--freq[c2-'a'];
 
-                // Reinsert both characters back 
-                if (f2 > 0) {
-                    heap[hz++]=c2;
-                    push_heap(heap, heap + hz);
-                }
-                heap[hz++] = c;
-                push_heap(heap, heap + hz);
-            } 
+                if (f2==0) pq.pop();
+
+                // Re-add the original character 
+                pq.emplace(c);
+            }
         }
+        
         return ans;
     }
 };
