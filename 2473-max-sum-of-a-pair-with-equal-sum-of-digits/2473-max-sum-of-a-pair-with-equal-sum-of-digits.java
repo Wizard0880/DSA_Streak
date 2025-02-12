46c44020412b5295d35d9760be48b1dca6f52498
Suryansh Kumar <95530953+Wizard0880@users.noreply.github.com>
2025-02-12 21:26:27 +0530

    Time: 31 ms (85.94%), Space: 55.7 MB (82.43%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,31 +1,38 @@
 class Solution {
     public int maximumSum(int[] nums) {
-
-        Arrays.sort(nums);
+        HashMap<Integer, int[]> map = new HashMap<>();
         int gsum = -1;
-        HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
 
-        for(int i : nums){
-            int sum = Digitsum(i);
-            map.putIfAbsent(sum, new ArrayList<>());
-            map.get(sum).add(i);
-        }
-
-        for(int i : map.keySet()){
-            ArrayList<Integer> al = map.get(i);
-            int l = al.size();
-            if(l > 1){
-                gsum = Math.max(gsum, al.get(l-1) + al.get(l-2));
+        for (int num : nums) {
+            int sum = digitSum(num);
+            
+            if (!map.containsKey(sum)) {
+                map.put(sum, new int[]{num, -1}); // Initialize with first number
+            } else {
+                int[] pair = map.get(sum);
+                if (num > pair[0]) {
+                    pair[1] = pair[0]; // Move largest to second largest
+                    pair[0] = num;
+                } else if (num > pair[1]) {
+                    pair[1] = num;
+                }
             }
         }
+
+        for (int[] pair : map.values()) {
+            if (pair[1] != -1) {
+                gsum = Math.max(gsum, pair[0] + pair[1]);
+            }
+        }
+
         return gsum;
     }
 
-    public int Digitsum(int num){
+    private int digitSum(int num) {
         int sum = 0;
-        while(num > 0){
-            sum = num % 10 + sum;
-            num = num / 10;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
         }
         return sum;
     }
