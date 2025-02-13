506cc324fd34594c59208b3503a1068a37f3e21a
Suryansh Kumar <95530953+Wizard0880@users.noreply.github.com>
2025-02-13 11:34:14 +0530

    Time: 2 ms (97.98%), Space: 45 MB (18.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -9,6 +9,7 @@ class Solution {
 
         Arrays.sort(arr1);
         Arrays.sort(arr2);
+        
         int n = arr1.length;
         for(int i = 0; i < n; i++){
             if(arr1[i] != arr2[i]){
