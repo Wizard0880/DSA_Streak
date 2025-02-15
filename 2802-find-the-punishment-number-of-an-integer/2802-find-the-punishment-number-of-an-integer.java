49aa15f490efc3e01f5b992b4b5cbb1cb6282788
Suryansh Kumar <95530953+Wizard0880@users.noreply.github.com>
2025-02-15 10:10:02 +0530

    Time: 109 ms (62.7%), Space: 44.2 MB (66.46%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -1,30 +1,34 @@
 class Solution {
     public int punishmentNumber(int n) {
         int sum = 0;
-        
         for (int i = 1; i <= n; i++) {
             int square = i * i;
-            if (canPartition(Integer.toString(square), i, 0, 0)) {
+            String s = Integer.toString(square);
+            if (canSplit(s, i)) {
                 sum += square;
             }
         }
-        
         return sum;
     }
     
-    private boolean canPartition(String s, int target, int index, int currentSum) {
-        if (index == s.length()) {
-            return currentSum == target;
+    private boolean canSplit(String s, int target) {
+        return helper(s, target, 0);
+    }
+    
+    private boolean helper(String s, int remaining, int start) {
+        if (start == s.length()) {
+            return remaining == 0;
         }
-        
-        int num = 0;
-        for (int j = index; j < s.length(); j++) {
-            num = num * 10 + (s.charAt(j) - '0');
-            if (canPartition(s, target, j + 1, currentSum + num)) {
+        for (int end = start + 1; end <= s.length(); end++) {
+            String part = s.substring(start, end);
+            int num = Integer.parseInt(part);
+            if (num > remaining) {
+                break;
+            }
+            if (helper(s, remaining - num, end)) {
                 return true;
             }
         }
-        
         return false;
     }
 }
